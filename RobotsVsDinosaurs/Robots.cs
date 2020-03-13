b8192e354868de4c2cb2c964bd683fd6f70bf18f
jorge bejarano <jorge.bejarano@yahoo.com>
2020-03-13 16:13:11 -0500

    initial commit

---
+++ RobotsVsDinosaurs/Robots.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotsVsDinosaurs
+{
+    class Robots
+    {
+        string name;
+        double health;
+        char powerlevel;
+        char weapon;
+        double attackPower;
+    }
+
+    public 
+    {
+        
+
+
+    }
+
+
+
+
+}
